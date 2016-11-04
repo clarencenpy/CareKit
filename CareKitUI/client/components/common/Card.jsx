598ac7e7c8d0e3b15902287fb5572d7252f3c1f4
clarencenpy <clarencenpy@gmail.com>
2016-11-04 01:37:12 -0400

    integrated jsplumb with some basic tests

---
+++ CareKitUI/client/components/common/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Button = (props) => (
+    <div id={props.id} className="ui bottom attached button">
+      <i className="add icon"/>
+      {props.text}
+    </div>
+)
+
+class Card extends React.Component {
+  render() {
+    return (
+        <div className="ui card jp-draggable">
+          <div className="content">
+            <div className="header">{this.props.header}</div>
+            <div className="description">
+              {this.props.description}
+            </div>
+          </div>
+          {
+            this.props.buttons && this.props.buttons.length > 0 ?
+                this.props.buttons.map((b) => (
+                    <Button {...b} key={b.id}/>
+                )) : null
+          }
+        </div>
+    )
+  }
+}
+
+export default Card
--- CareKitUI/client/components/common/Main.jsx
+++ CareKitUI/client/components/common/Main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import {findDOMNode as getDOM}  from 'react-dom'
+
+import Card from './Card'
 
 export default class Main extends React.Component {
   render() {
+    const buttons1 = [{id: '1-1', text: 'Action 1'}, {id: '1-2', text: 'Action 2'}]
+    const buttons2 = [{id: '2-1', text: 'Action 1'}, {id: '2-2', text: 'Action 2'}]
+    const buttons3 = [{id: '3-1', text: 'Action 1'}, {id: '3-2', text: 'Action 2'}]
     return (
-        <h1>Meteor + React + Semantic UI</h1>
+        <div id="jp-container" className="jp-container">
+          <Card ref="card1" header="Header" description="Hello this is pretty cool" buttons={buttons1}/>
+          <Card ref="card2" header="Header" description="Hello this is pretty cool" buttons={buttons2}/>
+          <Card ref="card3" header="Header" description="Hello this is pretty cool" buttons={buttons3}/>
+        </div>
     )
   }
 
+  componentDidMount() {
+    const jsPlumbInstance = jsPlumb.getInstance({
+      Container: 'jp-container'
+    })
+    jsPlumbInstance.connect({
+      source: '1-1',
+      target: getDOM(this.refs.card2),
+      anchors: ['Right', 'Top'],
+      endpoint: 'Dot'
+    })
+    jsPlumbInstance.connect({
+      source: '2-1',
+      target: getDOM(this.refs.card3),
+      anchors: ['Right', 'Top'],
+      endpoint: 'Dot'
+    })
+    jsPlumbInstance.draggable(
+        $('.jp-draggable')
+    )
+
+  }
 }
