8b14eed17e903907912ba93b20234539232a925b
Ryan Sickles <rsickles@andrew.cmu.edu>
2016-11-28 15:58:23 -0500

    Styled create card modal for text, image, and text. Started add input dynamically to create card modals

--- CareKitUI/client/components/common/CreateCardModal.jsx
+++ CareKitUI/client/components/common/CreateCardModal.jsx
@@ -2,31 +2,65 @@ import React from 'react'
 import uuid from 'uuid'
 import {findDOMNode as getDOM}  from 'react-dom'
 
-class CreateCardModal extends React.Component {
-  render() {
+var CreateCardModal = React.createClass ({
+
+    getInitialState: function() {
+    var id = uuid.v1()
+    var inputs = {
+      inputs: []
+    }
+    return inputs;
+  },
+    addOption: function(){
+      var new_id_number = this.state.length + 1
+      var new_option_number = this.state.length + 1
+      var id = uuid.v1()
+      this.setState(this.state.inputs.inputs.concat([
+              {id:new_id_number, name:new_option_number, uuid:{id}}
+          ]))
+  },
+  render: function() {
     return (
         <div className="ui modal">
-          <div className="header">Create Card</div>
+          <div className="header createcard"></div>
           <div className="content">
             <div className="ui input">
-              <input ref="message" type="text" placeholder="Enter Message..."/>
+              <input ref="message" type="text" placeholder="Enter Title"/>
             </div>
-            <div className="ui input">
-              <input ref="button1" type="text" placeholder="Action 1"/>
+            {
+                    this.state.inputs.map((item) => (
+                        <div className="ui input">
+                          <input ref={item.id} type="text" key={item.uuid} placeholder={item.name}/>
+                        </div>
+                    ))
+            }
+            <div className="audio-card-upload">
+            Upload Audio
+             <div className="audio-upload">
+              <input type="file" name="audio"></input>
+             </div>
             </div>
-            <div className="ui input">
-              <input ref="button2" type="text" placeholder="Action 2"/>
+            <div className="image-card-content">
+             Upload Image
+             <div className="image-upload">
+              <input type="file" name="pic" accept="image/*"></input>
+             </div>
+             <div className="ui input">
+              <textarea rows="4" cols="30" placeholder="Image Description"></textarea>
+             </div>
             </div>
           </div>
+           <div className="ui bottom attached button" onClick={this.addOption}>
+            <i className="plus icon"/> Add Button
+          </div>
           <div className="actions">
-            <div className="ui green approve button" onClick={this.addCard.bind(this)}>Create</div>
+            <div className="ui green approve button" onClick={this.addCard}>Create</div>
             <div className="ui red cancel button">Cancel</div>
           </div>
         </div>
-    )
-  }
-
-  addCard() {
+    );
+  },
+  addCard: function() {
     const id = uuid.v1()
     const message = getDOM(this.refs.message).value
     const button1Text = getDOM(this.refs.button1).value
@@ -37,6 +71,6 @@ class CreateCardModal extends React.Component {
       buttons: [{id: `${id}_button1`, text: button1Text}, {id: `${id}_button2`, text: button2Text}]
     })
   }
-}
+});
 
 export default CreateCardModal
--- CareKitUI/client/components/common/ToolsPalette.jsx
+++ CareKitUI/client/components/common/ToolsPalette.jsx
@@ -4,23 +4,23 @@ class ToolsPalette extends React.Component {
   render() {
     return (
         <div className="ui vertical labeled icon menu">
-          <a className="item" onClick={this.showCreateCardModal}>
+          <a className="item" onClick={() => this.showCreateCardModal("Text")}>
             <i className="file text outline icon" />
             Text
           </a>
-          <a className="item">
+          <a className="item" onClick={() => this.showCreateCardModal("Image")}>
             <i className="file image outline icon"/>
             Image
           </a>
-          <a className="item">
-            <i className="file audio outline icon"/>
+          <a className="item" onClick={() => this.showCreateCardModal("Audio")}>
+            <i className="file audio outline icon" />
             Audio
           </a>
-          <a className="item">
-            <i className="file video outline icon"/>
+          <a className="item" onClick={() => this.showCreateCardModal("Video")}>
+            <i className="file video outline icon" />
             Video
           </a>
-          <a className="item">
+          <a className="item" onClick={() => this.showCreateCardModal("List")}>
             <i className="list layout icon"/>
             List
           </a>
@@ -28,8 +28,21 @@ class ToolsPalette extends React.Component {
     )
   }
 
-  showCreateCardModal() {
-    $('.ui.modal').modal('show');
+  showCreateCardModal(type) {
+    $('.header.createcard').text("Create Card: " + type);
+    if(type!="Image"){
+      $(".image-card-content").hide();
+    }else{
+      $(".image-card-content").show();
+    }
+    if(type!="Audio"){
+      $(".audio-card-upload").hide();
+    }else{
+      $(".audio-card-upload").show();
+    }
+    $('.ui.modal').modal({
+    inverted: true
+  }).modal('show');
   }
 }
 
